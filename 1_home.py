56e56ba30b18b17969ba5d5a0a04554269091897
Leandro Fratel de Souza <leandrofratel@gmail.com>
2024-10-23 20:07:08 -0300

    Alteração do caminho

--- 1_home.py
+++ 1_home.py
@@ -9,7 +9,7 @@ st.set_page_config(
 )
 
 if "data" not in st.session_state:
-    df_data = pd.read_csv("datasets\CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = pd.read_csv(r"datasets//CLEAN_FIFA23_official_data.csv", index_col=0)
     df_data = df_data[ df_data["Contract Valid Until"] >= datetime.today().year ]
     df_data = df_data.sort_values(by="Overall", ascending=False)
     st.session_state["data"] = df_data
